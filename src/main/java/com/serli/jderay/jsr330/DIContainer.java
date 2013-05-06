0a75c54b6f268a254509a5da23600e894cd39ec5
cRiMi <julien@deray.fr>
2013-05-06 16:26:36 +0200

    Minor refactoring

--- src/main/java/com/serli/jderay/jsr330/DIContainer.java
+++ src/main/java/com/serli/jderay/jsr330/DIContainer.java
@@ -5,7 +5,10 @@
 package com.serli.jderay.jsr330;
 
 import com.serli.jderay.modules.Module;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,26 +19,27 @@ public class DIContainer {
     private ClassScanner classScanner;
     private InheritanceAnalyser inheritanceAnalyser;
     
+    private Map<String, Class<?>> classes;
+    private Map<String, Class<?>[]> inheritances;
+    
     public DIContainer() {
+        classes = new HashMap<>();
+        inheritances = new HashMap<>();
+        
         inheritanceAnalyser = new InheritanceAnalyser();
         classScanner = new ClassScanner();
     }
 
-    public void scan(Map<String, Module> listModules) throws MalformedURLException {
-        Map<String, Class<?>> classes;
-        Map<String, Class<?>[]> inheritances;
-    
-        classes = getClasses( listModules );
+    public void init(Map<String, Module> listModules) throws IOException, URISyntaxException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        scanModules( listModules );
         inheritances = inheritanceAnalyser.analyse( classes );
     }
 
-    private Map<String, Class<?>> getClasses(Map<String, Module> listModules) throws MalformedURLException {
+    private void scanModules(Map<String, Module> listModules) throws MalformedURLException {
         Map<URL, ClassLoader> urls = new HashMap<>();
-        for ( Module mod : listModules.values() ) {
+        for ( Module mod : listModules.values() )
             urls.put( mod.getUrl(), mod.getClassLoader() );
-        }
         
-        return classScanner.scanClasses(urls);
+        classes = classScanner.scanClasses(urls);
     }
-   
 }
